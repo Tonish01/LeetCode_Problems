bf1f45e01a15fb8efaa00a146436c53129a6b549
Tonish <122773804+Tonish01@users.noreply.github.com>
2024-02-17 01:15:34 +0530

    LeetCode

---
+++ Uniue_Morse_Code_Words.cpp
@@ -0,0 +1,92 @@
+/*
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+'a' maps to ".-",
+'b' maps to "-...",
+'c' maps to "-.-.", and so on.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+Return the number of different transformations among all words we have.
+
+ 
+
+Example 1:
+
+Input: words = ["gin","zen","gig","msg"]
+Output: 2
+Explanation: The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+There are 2 different transformations: "--...-." and "--...--.".
+Example 2:
+
+Input: words = ["a"]
+Output: 1
+ 
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 12
+words[i] consists of lowercase English letters.
+*/
+
+//----------------------------------------------------------------------------SOLUTION---------------------------------------------------------------------------
+
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& v) {
+         unordered_map<char,string> ump = {
+        {'a', ".-"},
+        {'b', "-..."},
+        {'c', "-.-."},
+        {'d', "-.."},
+        {'e', "."},
+        {'f', "..-."},
+        {'g', "--."},
+        {'h', "...."},
+        {'i', ".."},
+        {'j', ".---"},
+        {'k', "-.-"},
+        {'l', ".-.."},
+        {'m', "--"},
+        {'n', "-."},
+        {'o', "---"},
+        {'p', ".--."},
+        {'q', "--.-"},
+        {'r', ".-."},
+        {'s', "..."},
+        {'t', "-"},
+        {'u', "..-"},
+        {'v', "...-"},
+        {'w', ".--"},
+        {'x', "-..-"},
+        {'y', "-.--"},
+        {'z', "--.."}
+    };
+    
+    string word = "";
+    vector<string>vt;
+    for(int i=0; i<v.size(); i++){
+        string s = v[i];
+        for(int j=0; j<s.length(); j++){
+            word += ump[s[j]];
+        }
+        vt.push_back(word);
+        word.clear();
+    }
+    
+    set<string>st;
+    for(int i=0; i<vt.size(); i++){
+        st.insert(vt[i]);
+    }
+
+    return st.size();
+    }
+};
